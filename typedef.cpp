305ca849a40c68903e5c4a443a5ba0a0de23d5d0
Ahmet Muhammed Bayraktar <121517080+ahmetbyrktrr@users.noreply.github.com>
2023-03-14 18:44:15 +0300

    Add files via upload

---
+++ typedef.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+
+
+typedef std::string text_t ;
+typedef int number_t ;
+
+//using number_t = int;
+// using text_t = std:::string;
+
+int main(){
+	
+	text_t firstName = "Ahmet " ; 
+	number_t age = 21 ;
+	
+	
+	std:: cout << "Hello my name is " << firstName << '\n';
+	std:: cout << "I am " << age << " years old" ;
+	
+	
+	
+	
+	
+	return 0;
+}
