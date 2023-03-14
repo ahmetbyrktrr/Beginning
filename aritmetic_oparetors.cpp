dd645436eb9691affce3225feb2af8ae15f5c42c
Ahmet Muhammed Bayraktar <121517080+ahmetbyrktrr@users.noreply.github.com>
2023-03-14 18:44:35 +0300

    Add files via upload

---
+++ aritmetic_oparetors.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main(){
+	
+	int students = 30 ;
+	
+	//students = students + 1 ;
+	//students+=1;
+	//students++;
+	
+	//students = students - 1 ;
+	//students-=1;
+	//students--;
+	
+	//students = students * 2 ;
+	//students*=2;
+	
+	//students = students / 2;
+	//students/=2;
+	
+	
+	//std:: cout << students ;
+	
+	
+	int remainder = students % 4 ;
+	
+	std:: cout << remainder ;
+	
+	return 0;
+}
