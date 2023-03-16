66e4e63a46866519e2a860937acd4e16cf6a8e11
Ahmet Muhammed Bayraktar <121517080+ahmetbyrktrr@users.noreply.github.com>
2023-03-16 18:47:10 +0300

    Add files via upload

---
+++ type_conversion.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+int main(){
+	
+	int correct = 7;
+	int questions = 10;
+	double grade = correct / (double)questions * 100;
+	
+	std::cout << grade << "%" ;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
