8de41f632790ce9d7753666f4237ce4f1914c8f0
Ahmet Muhammed Bayraktar <121517080+ahmetbyrktrr@users.noreply.github.com>
2023-03-16 18:47:35 +0300

    Add files via upload

---
+++ input.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+
+
+int main(){
+	
+	std::string name ;
+	int age ;
+	
+	
+	std:: cout << "What's your name ? " ;
+	std:: cin >> name ; 
+	std:: cout << "What's your age ? " ;
+	std :: cin >> age ; 
+	std:: cout << "Hello " << name << '\n';
+	std:: cout << "You're " << age << " years old" ;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
