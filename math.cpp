58404f6c6fb2bc83ae85f01f139fa27ec988078c
Ahmet Muhammed Bayraktar <121517080+ahmetbyrktrr@users.noreply.github.com>
2023-03-18 19:17:57 +0300

    Add files via upload

---
+++ math.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+	
+	double x = 3.49 ;
+	double y = 4;
+	double z ;
+	
+	//z = std::max(x, y);
+	//z = std::min (x, y);
+	//z = pow(2,3);
+	//z = sqrt(225);
+	//z = abs(-6);
+	//z = floor (x);
+	//z = ceil (x);
+	//z = round (x);
+	
+	
+	std::cout << z;
+	
+	
+	return 0;
+}
+
+
+https://cplusplus.com/reference/cmath/
