3b83f94142da9fd4cde7863b0022b67dcff7e855
Ahmet Muhammed Bayraktar <121517080+ahmetbyrktrr@users.noreply.github.com>
2023-03-20 20:32:06 +0300

    Add files via upload

---
+++ logical_operator.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+int main(){
+	
+	//&& and
+	//|| or
+	// ! not
+	
+	int temp;
+	
+	//and operator
+	//std::cout << "Enter the temperature: ";
+	//std::cin >> temp;
+	
+	//if (temp >0 && temp < 30){
+	//	std::cout << "The temperature is good! ";
+	//}
+	//else {
+	//	std::cout << "The temperature is bad." ;
+	//}
+	
+	//or operator
+	//std::cout << "Enter the temperature: ";
+	//std::cin >> temp;
+	//if (temp <= 0 || temp >= 30){
+	//	std::cout << "The temperature is bad! ";
+	//}
+	//else {
+	//	std::cout << "The temperature is good." ;
+	//}
+	
+	
+	
+
+	return 0;
+}
