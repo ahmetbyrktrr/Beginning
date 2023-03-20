9e5a7847328b415fe0a4c5cb629bfcc439014178
Ahmet Muhammed Bayraktar <121517080+ahmetbyrktrr@users.noreply.github.com>
2023-03-20 20:31:51 +0300

    Add files via upload

---
+++ ternary.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+	
+	// ternary operator = ?:
+	//condition ? experssion1 : experssion2 ;
+	
+	int number = 8;
+	
+	number % 2 ==1 ? std::cout << "ODD" : std::cout << "EVEN" ;
+	
+
+	return 0;
+	
+}
