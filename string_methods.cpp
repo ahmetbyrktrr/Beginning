b8d65fe4f1329ff26df6fb0c729f3593aa230b2c
Ahmet Muhammed Bayraktar <121517080+ahmetbyrktrr@users.noreply.github.com>
2023-03-24 02:59:29 +0300

    Add files via upload

---
+++ string_methods.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	
+	string name;
+	
+	cout << "Enter your name: ";
+	getline(std::cin, name);
+	
+	//if (name.length() > 12 ) {
+	//	cout << "Your name can't be over 12 characters" ; 
+	//}
+	//else {
+	//	cout << "Welcome " << name ;
+	//}
+	
+	
+	
+	//if (name.empty()){
+	//	cout << "You didn't enter your name.";
+	//}
+	//else {
+	//	cout << "Hello " << name;
+	//}
+	
+	
+	//name.clear();
+	//cout << "Hello " << name ;
+	
+	
+	//name.append("@hotmail.com");
+	//cout << "Your user name is " << name;
+	
+	
+	//cout << name.at(3);
+	
+	
+	//name.insert(0, "@");
+	//cout << name;
+	
+	
+	//name.erase(0, 4);
+	//cout << name;
+	
+	
+	https://cplusplus.com/reference/string/string/
+	
+	
+	return 0;
+}
